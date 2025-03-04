1995d296d9757adc0ca93bc8200af79b6ab79f96
Mukul Pal <mukulpal0333@gmail.com>
2025-03-04 08:31:38 +0530

    Variables

---
+++ 02 - Variables/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    // cout << "Hello World" << endl; :- comments
+
+    int score;
+    score = 110;
+
+    int balance = 500;
+
+    const int uid = 232323;
+    // uid = 1212;  error
+}

1a0c43a494058ed47617fb2229c7ac9c920bdc00
Mukul Pal <mukulpal0333@gmail.com>
2025-03-05 08:42:27 +0530

    Strings

---
+++ 03 - Primitives/string.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main () {
+    string fav_tea = "Lemon tea";
+    string description = "Known as \"best\" tea";  // escape character
+
+    return 0;
+}

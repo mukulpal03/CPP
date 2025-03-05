7e5867cf0165f7816d62247eb57b3fa8e11529dd
Mukul Pal <mukulpal0333@gmail.com>
2025-03-05 08:31:15 +0530

    Modifiers

---
+++ 03 - Primitives/modifier.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    unsigned int small_tea_pack = -100;  // only positive values are allowed, now if we pass -100 we get a garbage value
+
+    long large_tea_storage = 10000000;
+
+    short tea_sample = 25;
+
+    return 0;
+}

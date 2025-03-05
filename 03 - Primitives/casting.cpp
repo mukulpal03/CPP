052636bf0700d71cdd2b8614d999e3420964fadf
Mukul Pal <mukulpal0333@gmail.com>
2025-03-05 08:36:21 +0530

    Type Casting

---
+++ 03 - Primitives/casting.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    float tea_price = 49.99;
+    int rounded_tea_price = (int) tea_price;
+
+    int tea_quantity = 2;
+    double totalPrice = tea_price * tea_quantity;
+
+    return 0;
+}

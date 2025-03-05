398f5d9116afbae8704eb2ba729eeaeb3fc8d9ae
Mukul Pal <mukulpal0333@gmail.com>
2025-03-05 08:23:25 +0530

    Datatypes

---
+++ 03 - Primitives/datatype.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    int tea_leaves = 50;
+    float water_temperature = 85.5;
+    double price_of_tea = 299.99;
+    char tea_grade = 'A';
+    // bool is_tea_ready = 5;
+    // cout << is_tea_ready;  // output - 1 (true equivalent)
+    bool is_tea_ready = false;
+
+    return 0;
+}

b9be320af28a23794cb9d34048c2d05de3ea232f
Mukul Pal <mukulpal0333@gmail.com>
2025-03-07 12:07:45 +0530

    If-else statement

---
+++ 05_conditionals/task2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    int hour;
+
+    cout << "Enter the current hour (0 - 23)" << endl;
+    cin >> hour;
+
+    if(hour >= 8 && hour <= 18) {
+        cout << "Tea shop is open" << endl;
+    } else {
+        cout << "Tea shop is closed" << endl;
+    }
+}

124c39463b6e56257b281b7bba25b6e0a874df52
Mukul Pal <mukulpal0333@gmail.com>
2025-03-08 13:13:16 +0530

    For Loop

---
+++ 06_Loops/for.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    int tea_cups = 5;
+
+    for(int i = 0; i < tea_cups; i++) {
+        cout << "Brewing" << i << "cup of tea" << endl;
+    }
+}
